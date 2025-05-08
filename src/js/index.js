297c0530d81a57d7aea45e7d1f39fce549188f45
Katja Turnsek <158604683+KatjaTurnsek@users.noreply.github.com>
2025-05-08 10:38:45 +0200

    Loader
    Loaded added with updated HTML, CSS and JS functions.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import { updateSwitcherPosition } from "./toggle.js";
+import { hideLoader, showLoader } from "./loader.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  showLoader();
+
+  // Always hide after delay (simulate content load)
+  setTimeout(() => {
+    hideLoader();
+  }, 3000);
+});
