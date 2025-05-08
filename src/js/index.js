05975e50350b972099a6c0662ac71a945a97263b
Katja Turnsek <158604683+KatjaTurnsek@users.noreply.github.com>
2025-05-08 20:14:40 +0200

    update
    Header and components

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import { updateSwitcherPosition } from "./toggle.js";
 import { hideLoader, showLoader } from "./loader.js";
+import { setupMenuToggle } from "./nav.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupMenuToggle();
+});
 
 window.addEventListener("DOMContentLoaded", () => {
   showLoader();
@@ -8,4 +13,13 @@ window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     hideLoader();
   }, 3000);
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,1 +1,18 @@
+export function setupMenuToggle() {
+  const menuToggle = document.getElementById("menuToggle");
+  const menuClose = document.getElementById("menuClose");
+  const menu = document.getElementById("menu");
 
+  function openMenu() {
+    menu.classList.add("open");
+    menuToggle.style.display = "none";
+  }
+
+  function closeMenu() {
+    menu.classList.remove("open");
+    menuToggle.style.display = "inline-block";
+  }
+
+  menuToggle.addEventListener("click", openMenu);
+  menuClose.addEventListener("click", closeMenu);
+}
