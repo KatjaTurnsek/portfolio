0fc8f7d4a978cb39f1a71f9e2c3105aacc610c37
Katja Turnsek <158604683+KatjaTurnsek@users.noreply.github.com>
2025-05-10 19:07:12 +0200

    toggle update

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,12 @@ import { updateSwitcherPosition } from "./toggle.js";
 import { hideLoader, showLoader } from "./loader.js";
 import { setupMenuToggle } from "./nav.js";
 
+// Set up menu toggle functionality
 document.addEventListener("DOMContentLoaded", () => {
   setupMenuToggle();
 });
 
-// Loader
+// Loader functionality
 window.addEventListener("DOMContentLoaded", () => {
   showLoader();
 
@@ -15,6 +16,7 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 3000);
 });
 
+// Scroll effect on header
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   if (window.scrollY > 10) {
@@ -22,36 +24,4 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("scrolled");
   }
-});
-
-// logo
-const themeToggle = document.getElementById("theme-toggle");
-const siteLogos = document.querySelectorAll(".site-logo");
-
-function updateLogo() {
-  const isDark = document.body.classList.contains("dark-theme");
-  document.querySelectorAll(".site-logo").forEach((logo) => {
-    logo.src = isDark
-      ? "assets/images/logo-katjadev-light.svg"
-      : "assets/images/logo-katjadev-dark.svg";
-  });
-}
-
-// Set theme based on system preference if none is set
-if (
-  !document.documentElement.classList.contains("dark-theme") &&
-  !document.documentElement.classList.contains("light-theme")
-) {
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  document.documentElement.classList.add(
-    prefersDark ? "dark-theme" : "light-theme"
-  );
-}
-
-updateLogo();
-
-themeToggle.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark-theme");
-  document.documentElement.classList.toggle("light-theme");
-  updateLogo();
 });
