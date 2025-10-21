f17d74b52ee45bafcc4924e5da8b27a91eb32905
Katja Turnsek <158604683+KatjaTurnsek@users.noreply.github.com>
2025-10-21 09:44:42 +0200

    Deploy: 2025-10-21T07:44:42Z

--- src/lib/render.js
+++
@@ -1,8 +0,0 @@
-export function html(strings, ...values) {
-  return strings.reduce((out, s, i) => out + s + (values[i] ?? ''), '');
-}
-export function render(target, markup) {
-  const el = typeof target === 'string' ? document.querySelector(target) : target;
-  if (!el) throw new Error('Target not found');
-  el.innerHTML = markup;
-}
